ac4e49790c1b90b3564b043df884415e63f86f26
Olga Ibakaeva <ibakaeva2008@yandex.ru>
2022-08-26 08:51:54 +0500

    Rename TestClassCatMethodGetFood

--- src/test/java/testsClassCat/TestClassCatMethodGetFood.java
+++ src/test/java/testsClassCat/TestClassCatMethodGetFood.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
-public class testClassCatMethodGetFood {
+public class TestClassCatMethodGetFood {
     @Mock
     Feline feline;
 
---
+++ src/test/java/testsClassLion/TestClassLionMethodGetKittens.java
@@ -0,0 +1,4 @@
+package testsClassLion;
+
+public class TestClassLionMethodGetKittens {
+}
